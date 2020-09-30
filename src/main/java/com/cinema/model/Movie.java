019a70989d167d8e6c8e5d2b2efd601e07703ec4
FYarkiy <flx_jb@yahoo.com>
2020-09-30 16:23:54 +0300

    add annotation @table, remove empty constructor in Movie. remove unused dependencies in pom.xml

--- src/main/java/com/cinema/model/Movie.java
+++ src/main/java/com/cinema/model/Movie.java
@@ -5,17 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "movie")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String descritpion;
-
-    public Movie() {
-    }
 
     public Long getId() {
         return id;
