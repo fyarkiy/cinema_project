ad23e56e5c0ce28ec928db06901cd12db1b7491c
FYarkiy <flx_jb@yahoo.com>
2020-10-06 19:20:10 +0300

    remove unused imports

--- src/main/java/com/cinema/service/CinemaHallService.java
+++ src/main/java/com/cinema/service/CinemaHallService.java
@@ -1,6 +1,5 @@
 package com.cinema.service;
 
-import com.cinema.dao.GenericDao;
 import com.cinema.model.CinemaHall;
 
 public interface CinemaHallService extends GenericService<CinemaHall> {
--- src/main/java/com/cinema/service/MovieService.java
+++ src/main/java/com/cinema/service/MovieService.java
@@ -1,6 +1,5 @@
 package com.cinema.service;
 
-import com.cinema.dao.GenericDao;
 import com.cinema.model.Movie;
 
 public interface MovieService extends GenericService<Movie> {
