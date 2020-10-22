479b045877bcb2d28f91496b588d5d733db04ac1
FYarkiy <flx_jb@yahoo.com>
2020-10-22 17:33:45 +0300

    Create Auth Controller

---
+++ src/main/java/com/cinema/controllers/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.cinema.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cinema")
+public class AuthenticationController {
+}
